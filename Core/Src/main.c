33e7d27b69330d1eb25e60731d416ad2ad2e8dbf
Ken <kqr2@hotmail.com>
2022-05-16 21:09:19 -0700

    Use BSP to initialize LCD; comment out STM32Cube MX init code except for UART as the BSP initializes LTDC and sdram

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -117,18 +117,19 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_CRC_Init();
-  MX_DMA2D_Init();
-  MX_FMC_Init();
-  MX_I2C3_Init();
-  MX_LTDC_Init();
-  MX_SPI5_Init();
-  MX_TIM1_Init();
+  //MX_GPIO_Init();
+  //MX_CRC_Init();
+  //MX_DMA2D_Init();
+  //MX_FMC_Init();
+  //MX_I2C3_Init();
+  //MX_LTDC_Init();
+  //MX_SPI5_Init();
+  //MX_TIM1_Init();
   MX_USART1_UART_Init();
-  MX_USB_HOST_Init();
-  MX_ADC1_Init();
+  //MX_USB_HOST_Init();
+  //MX_ADC1_Init();
   //MX_FATFS_Init();
+
   /* USER CODE BEGIN 2 */
 
   // https://embedded.fm/blog/2017/1/19/discovery-uart-input
@@ -139,26 +140,113 @@ int main(void)
   // Initialize command console
   ConsoleInit();
 
+  // Initialize LCD
+  BSP_LCD_Init();
+
+#define LCD_FRAME_BUFFER_LAYER0                  (LCD_FRAME_BUFFER+0x130000)
+#define LCD_FRAME_BUFFER_LAYER1                  LCD_FRAME_BUFFER
+
+  /* Layer2 Init */
+  BSP_LCD_LayerDefaultInit(1, LCD_FRAME_BUFFER_LAYER1);
+  /* Set Foreground Layer */
+  BSP_LCD_SelectLayer(1);
+
+  /* Clear the LCD */
+  BSP_LCD_Clear(LCD_COLOR_WHITE);
+  BSP_LCD_SetColorKeying(1, LCD_COLOR_WHITE);
+  BSP_LCD_SetLayerVisible(1, DISABLE);
+
+  /* Layer1 Init */
+  BSP_LCD_LayerDefaultInit(0, LCD_FRAME_BUFFER_LAYER0);
+  /* Set Foreground Layer */
+  BSP_LCD_SelectLayer(0);
+
+  /* Enable The LCD */
+  BSP_LCD_DisplayOn();
+
+  /* Clear the LCD */
+  BSP_LCD_Clear(LCD_COLOR_WHITE);
+
+  BSP_LCD_SetTextColor(LCD_COLOR_DARKGRAY);
+  BSP_LCD_SetFont(&Font16);
+
+  BSP_LCD_DisplayStringAtLine(1, (uint8_t *)"ABCD");
+  BSP_LCD_DisplayStringAtLine(2, (uint8_t *)"EFGH");
+  BSP_LCD_DisplayStringAtLine(3, (uint8_t *)"IJKL");
+
   /* USER CODE END 2 */
 
-  // https://embedded.fm/blog/2017/1/19/discovery-uart-input
-  // Disable buffering of serial ports
-  setvbuf(stdin, NULL, _IONBF, 0);
-  setvbuf(stdout, NULL, _IONBF, 0);
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
     /* USER CODE END WHILE */
-    MX_USB_HOST_Process();
+    //MX_USB_HOST_Process();
 
     /* USER CODE BEGIN 3 */
     ConsoleProcess();
+
     HAL_Delay(2);
   }
   /* USER CODE END 3 */
 }
 
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 168000000
+  *            HCLK(Hz)                       = 168000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 336
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 5
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /* The voltage scaling allows optimizing the power consumption when the device is
+     clocked below the maximum system frequency, to update the voltage scaling value
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+}
+
+#if 0
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -203,6 +291,7 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 }
+#endif
 
 /**
   * @brief ADC1 Initialization Function
